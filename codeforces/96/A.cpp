040e309c9d98edd1e55b0e4822a5e8e3d45732b4
Shahil Singh <shahilsingh5501@gmail.com>
2020-02-19 02:33:00 +0530

    Add solution for problem `A - Football` Link: https://codeforces.com/contest/96/problem/A Tags: implementation, strings, *900 Ref: https://codeforces.com/contest/96/submission/71404228

--- codeforces/96/A.cpp
+++ codeforces/96/A.cpp
@@ -1,71 +1,58 @@
-//shahil_005
-//Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+    //shahil_005
+    //Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
 
-#include <bits/stdc++.h>
+    #include <bits/stdc++.h>
 
-using namespace std;
+    using namespace std;
 
-#define endl '\n'
-#define int long long
-#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+    #define endl '\n'
+    #define int long long
+    #define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
-const int N = 1e5 + 5;
-const long long MOD=(long long)(1e9+7);
-const long long INIT=(long long)(1e6+1);
+    const int N = 1e5 + 5;
+    const long long MOD=(long long)(1e9+7);
+    const long long INIT=(long long)(1e6+1);
 
-int32_t main()
-{
-    string s;
-    cin>>s;
-    int n=s.size();
-    int count=1;
-    for(int i=1;i<n;i++)
+    int32_t main()
     {
-    	/*if(s[i]==current)
-    	{
-    		count++;
-		}
-		else
-		{
-			if(count>=7)
-			{
-				cout<<"YES";
-				return 0;
-			}
-			count=1;
-			current=s[i];
-		}*/
+        string s;
+        cin >> s;
 
-		if(s[i] == s[i-1])
+        int n= s.size();
+
+        if(n<7)
         {
-            count++;
+            cout << "NO";
         }
 
         else
         {
-            count =1;
+            int sum;
+            for(int i=0;i<=n-7;i++)
+            {
+                sum = s[i] + s[i+1] + s[i+2] + s[i+3] + s[i+4] + s[i+5] + s[i+6];
+                if(sum == 336 || sum == 343)
+                {
+                    break;
+                }
+            }
+
+            if(sum == 336)
+            {
+                cout << "YES";
+            }
+
+            else if(sum == 343)
+            {
+                cout << "YES";
+            }
+
+            else
+            {
+                cout << "NO";
+            }
+
         }
-        if(count == 7)
-        {
-            cout << "YES";
-            break;
-        }
 
-
-    }
-
-
-
-	/*if(count>=7)
-    {
-        cout<<"YES";
         return 0;
-    }*/
-
-    if(count < 7)
-    {
-        cout<<"NO";
     }
-
-	return 0;
-}
