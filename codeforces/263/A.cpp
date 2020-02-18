c1b266c4b22b1fdc92a9f968960f18ae7c2021ac
Shahil Singh <shahilsingh5501@gmail.com>
2020-02-19 03:34:00 +0530

    Add solution for problem `A - Beautiful Matrix` Link: https://codeforces.com/contest/263/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/263/submission/71405711

---
+++ codeforces/263/A.cpp
@@ -0,0 +1,37 @@
+//shahil_005
+//Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define int long long
+#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const int N = 1e5 + 5;
+const long long MOD=(long long)(1e9+7);
+const long long INIT=(long long)(1e6+1);
+
+int32_t main()
+{
+    int a[5][5];
+    int x,y;
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            cin >> a[i][j];
+            if(a[i][j] == 1)
+            {
+                cout << abs(i-2) + abs(j-2);
+                break;
+            }
+        }
+    }
+
+    
+
+
+	return 0;
+}
