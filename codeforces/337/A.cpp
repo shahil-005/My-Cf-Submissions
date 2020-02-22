c11cce480ec1fc38b87fe1700c85de13ae62c92a
Shahil Singh <shahilsingh5501@gmail.com>
2020-02-22 15:07:00 +0530

    Add solution for problem `A - Puzzles` Link: https://codeforces.com/contest/337/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/337/submission/71602269

---
+++ codeforces/337/A.cpp
@@ -0,0 +1,38 @@
+//shahil_005
+//Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define int long long
+#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const int N = 1e5 + 5;
+const long long MOD=(long long)(1e9+7);
+const long long INIT=(long long)(1e6+1);
+
+int32_t main()
+{
+    int n,m;
+    cin >> m >> n;
+
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+    sort(a,a+n);
+    int dif = INT_MAX;
+    for(int i=0;i<=n-m ;i++)
+    {
+        if((a[i+m-1] - a[i])< dif)
+        {
+            dif = a[i+m-1] - a[i];
+        }
+    }
+    cout << dif;
+    return 0;
+}
+
