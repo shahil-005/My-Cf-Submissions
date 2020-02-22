9ed5bb78fbc6fb421c424279711626db4596da10
Shahil Singh <shahilsingh5501@gmail.com>
2020-02-22 07:30:00 +0530

    Add solution for problem `A - Word` Link: https://codeforces.com/contest/59/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/59/submission/71581613

---
+++ codeforces/59/A.cpp
@@ -0,0 +1,48 @@
+//shahil_005
+//Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define int long long
+#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const int N = 1e5 + 5;
+const long long MOD=(long long)(1e9+7);
+const long long INIT=(long long)(1e6+1);
+
+int32_t main()
+{
+    string s;
+    cin >>s;
+    int n= s.size();
+    int a=0,b=0;
+    for(int i=0;i<n;i++)
+    {
+        if(isupper(s[i]))
+        {
+            a++;
+        }
+
+        if(islower(s[i]))
+        {
+            b++;
+        }
+    }
+
+    if(a>b)
+    {
+        transform(s.begin(),s.end(),s.begin(),::toupper);
+    }
+
+    else if(a<=b)
+    {
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+    }
+
+    cout << s;
+    return 0;
+}
+
