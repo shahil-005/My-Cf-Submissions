390263d0aaf29b813cf23e45f3b61df1985e5363
Shahil Singh <shahilsingh5501@gmail.com>
2020-02-24 17:40:00 +0530

    Add solution for problem `A - Insomnia cure` Link: https://codeforces.com/contest/148/problem/A Tags: constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/148/submission/71761758

---
+++ codeforces/148/A.cpp
@@ -0,0 +1,38 @@
+//shahil_005
+// Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define fr(i,s,e) for(i=s;i<e;i++)
+#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const int N = 1e5 + 5;
+const long long MOD=(long long)(1e9+7);
+const long long INIT=(long long)(1e6+1);
+
+int32_t main()
+{
+    int k,l,m,n,d;
+    cin>>k>>l>>m>>n>>d;
+    if(k==1||l==1||m==1||n==1)
+    {
+        cout<<d<<endl;
+    }
+    else
+    {
+        int ans=0;
+        for(int i=1;i<=d;i++)
+        {
+            if(i%k==0||i%l==0||i%m==0||i%n==0)
+            {
+                ans++;
+            }
+        }
+        cout << ans <<endl;
+    }
+    return 0;
+}
+
