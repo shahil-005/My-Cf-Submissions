412a18f6fd0ee5806c7c0f28f90aacd93cf138f2
Shahil Singh <shahilsingh5501@gmail.com>
2020-02-25 16:54:00 +0530

    Add solution for problem `B - Drinks` Link: https://codeforces.com/contest/200/problem/B Tags: implementation, math, *800 Ref: https://codeforces.com/contest/200/submission/71852702

---
+++ codeforces/200/B.cpp
@@ -0,0 +1,34 @@
+//shahil_005
+// Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+const int N = 1e5 + 5;
+const long long MOD=(long long)(1e9+7);
+const long long INIT=(long long)(1e6+1);
+
+int n;
+vector<int> v;
+int main()
+{   IOS;
+
+    int n;
+    cin >> n;
+    float a[n];
+    float sum=0;
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i];
+        sum+= a[i];
+    }
+    std::cout << std::fixed;
+    std::cout << std::setprecision(12);
+    cout << sum/n;
+
+    return 0;
+}
