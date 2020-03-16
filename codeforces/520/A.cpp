8469c7a6ad6d1f376b27f8c4efdb6f80381c881e
Shahil Singh <shahilsingh5501@gmail.com>
2020-03-16 17:12:00 +0530

    Add solution for problem `A - Pangram` Link: https://codeforces.com/contest/520/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/520/submission/73403596

---
+++ codeforces/520/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+int32_t main()
+{   IOS;
+
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+   set<string> se;
+    
+    for(int i=0;i<n;i++)
+    {
+        string r = "";
+        r += s[i];
+        se.insert(r);
+    }
+    /*for(auto it:m)
+    {
+        cout << it.first << " " << it.second << endl;
+    }*/
+    int x = se.size();
+    if(x == 26)
+        cout <<"YES";
+        
+    else
+        cout <<"NO";
+    return 0;
+}
