81de046a7259049e20bb51644f4e9a8ebf23aeae
Shahil Singh <shahilsingh5501@gmail.com>
2020-03-16 17:33:00 +0530

    Add solution for problem `A - Amusing Joke` Link: https://codeforces.com/contest/141/problem/A Tags: implementation, sortings, strings, *800 Ref: https://codeforces.com/contest/141/submission/73405104

--- codeforces/141/A.cpp
+++ codeforces/141/A.cpp
@@ -11,13 +11,36 @@ int32_t main()
 
     string a,b,c;
     cin >> a >> b >> c;
-    string d = a+b;
-    
-    sort(c.begin(),c.end());
-    sort(d.begin(),d.end());
-    if(c==d)
+    int x[27] ={0};
+    int y[27] ={0};
+    int z[27] ={0};
+
+    for(int i=0;i<a.size();i++)
+    {
+        x[a[i]-64]++;
+    }
+    for(int i=0;i<b.size();i++)
+    {
+        y[b[i]-64]++;
+    }
+    for(int i=0;i<c.size();i++)
+    {
+        z[c[i]-64]++;
+    }
+
+    int flag =0;
+    for(int i=1;i<=26;i++)
+    {
+        if(x[i] + y[i] !=z[i])
+        {
+            cout << "NO";
+            flag =1;
+            break;
+        }
+    }
+    if(flag ==0)
+    {
         cout << "YES";
-    else
-        cout << "NO";
+    }
     return 0;
 }
