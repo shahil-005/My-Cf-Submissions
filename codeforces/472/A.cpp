f2df6f6992b645d3c02706bd3704a3ca8973eadd
Shahil Singh <shahilsingh5501@gmail.com>
2020-03-17 05:52:00 +0530

    Add solution for problem `A - Design Tutorial: Learn from Math` Link: https://codeforces.com/contest/472/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/472/submission/73446411

---
+++ codeforces/472/A.cpp
@@ -0,0 +1,29 @@
+//shahil_005
+// Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define ll long long
+#define ld long double
+#define f(i , s , n) for(ll i = s ; i < n ; i++)
+#define fe(i , s , n) for(ll i = s ; i <= n ; i++)
+
+const int N = 1e5 + 5;
+const long long MOD=(long long)(1e9+7);
+const long long INIT=(long long)(1e6+1);
+
+int main()
+{   IOS;
+
+    ll n;
+    cin >> n;
+
+    (n&1) ? cout << 9 << " " << n-9 : cout << 4 << " " << n-4;
+    
+      
+    return 0;
+}
