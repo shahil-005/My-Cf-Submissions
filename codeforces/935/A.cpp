6bce1fb599ba6d9456afd58c5f278d54d2af7797
Shahil Singh <shahilsingh5501@gmail.com>
2020-03-17 06:52:00 +0530

    Add solution for problem `A - Fafa and his Company` Link: https://codeforces.com/contest/935/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/935/submission/73447742

---
+++ codeforces/935/A.cpp
@@ -0,0 +1,35 @@
+//shahil_005
+// Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define ll long long
+#define ld long double
+#define f(i , s , n) for(ll i = s ; i < n ; i++)
+#define fe(i , s , n) for(ll i = s ; i <= n ; i++)
+
+const int N = 1e5 + 5;
+const long long MOD=(long long)(1e9+7);
+const long long INIT=(long long)(1e6+1);
+
+int main()
+{   IOS;
+
+    ll n;
+    cin >> n;
+    ll ans=0;
+    fe(i,1,n/2)
+    {
+        if((n-i)%i == 0){
+            ans++;
+            //cout << i << endl;
+        }
+
+    }
+    cout << ans << endl;
+    return 0;
+}
