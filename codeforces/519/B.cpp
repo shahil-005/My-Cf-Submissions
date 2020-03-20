460d50ac0ed7022d7c6eafce69786af201176774
Shahil Singh <shahilsingh5501@gmail.com>
2020-03-20 20:11:00 +0530

    Add solution for problem `B - A and B and Compilation Errors` Link: https://codeforces.com/contest/519/problem/B Tags: data structures, implementation, sortings, *1100 Ref: https://codeforces.com/contest/519/submission/73802705

--- codeforces/519/B.cpp
+++ codeforces/519/B.cpp
@@ -1,31 +1,104 @@
+//shahil_005
+// Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+///////////////////////////////////////////////"GO CORONA GO"///////////////////////////////////////////////
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main()
+//Defines:-------------------------------------------------------------------------------------------------------------
+#define endl        '\n'
+//#define IOS       ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define loop        for(ll i = 0 ; i < n ; i++)
+#define f(i,s,n)    for(ll i = s ; i < n ; i++)
+#define fe(i,s,n)   for(ll i = s ; i <= n ; i++)
+#define tc(x)       ll x; cin>>x; while(x--)
+#define auto(m)     for(auto &it:m)
+//#define ll        long long
+//#define ld        long double
+//#define vl        vector<ll>
+#define mii         map<ll, ll>
+#define mci         map<char, ll>
+#define msi         map<string, ll>
+#define mp          make_pair
+#define pii         pair<ll, ll>
+#define pb          push_back
+#define ff          first
+#define ss          second
+#define all(v)      v.begin(), v.end()
+//#define sort(v)     sort(v.begin(), v.end())
+#define rsort(v)    sort(v.begin(), v.end(),greater<ll>())
+#define Max(x,y,z)  max(x,max(y,z))
+#define Min(x,y,z)  min(x,min(y,z))
+//#define ps(x,y)     fixed<<setprecision(y)<<x
+//#define ps(x)       std::cout << std::fixed; std::cout << std::setprecision(x);
+#define clz(a)      __builtin_clz(a) // count leading zeroes (geeksforgeeks.org/builtin-functions-gcc-compiler/)
+#define ctz(a)      __builtin_ctz(a) // count trailing zeroes
+#define popc(a)     __builtin_popcount(a) // count set bits (for ints only diff for ll)
+
+//Typedefs:-------------------------------------------------------------------------------------------------------------
+typedef long long ll;
+typedef long double ld;
+//typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+
+const int N = 1e5 + 5;
+const long long MOD=(long long)(1e9+7);
+const long long INIT=(long long)(1e6+1);
+
+//Functions:------------------------------------------------------------------------------------------------------------
+
+ll power(ll x,ll y)
 {
-    int N,temp;
-    cin>>N;
-    vector<int>a,b,c;
-    for(int i=0;i<N;i++){
-        cin>>temp;
-        a.push_back(temp);
+	if (y == 0)
+		return 1;
+	ll p = power(x, y/2) % MOD;
+	p = (p * p) % MOD;
+
+	return (y%2 == 0)? p : (x * p) % MOD;
+}
+
+//Main:-----------------------------------------------------------------------------------------------------------------
+int main ()
+{
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+//----------------------------------------------------------------------------------------------------------------------
+
+    ll n;
+    cin>>n;
+    ll a[n],b[n-1],c[n-2];
+    f(i,0,n)
+    {
+        cin>>a[i];
     }
-    for(int i=0;i<N-1;i++){
-        cin>>temp;
-        b.push_back(temp);
+    f(i,0,n-1)
+    {
+        cin>>b[i];
     }
-    for(int i=0;i<N-2;i++){
-        cin>>temp;
-        c.push_back(temp);
+    f(i,0,n-2)
+    {
+        cin>>c[i];
     }
-    sort(a.begin(),a.end());
-    sort(b.begin(),b.end());
-    sort(c.begin(),c.end());
-    vector<int>x(1);
-    vector<int>y(1);
-    auto p=set_difference(a.begin(),a.end(),b.begin(),b.end(),x.begin());
-    auto q=set_difference(b.begin(),b.end(),c.begin(),c.end(),y.begin());
-    cout<<x[0]<<endl<<y[0];
+    ll sum1=0,sum2=0,sum3=0;
+    sum1 = accumulate(a,a+n,sum1);
+    sum2 = accumulate(b,b+n-1,sum2);
+    sum3 = accumulate(c,c+n-2,sum3);
+    cout<<sum1-sum2<<endl;
+    cout<<sum2-sum3<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
     return 0;
 }
