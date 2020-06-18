24ac8cb1907b239e8a7bfdd11e325ec2252a5464
Shahil Singh <shahilsingh5501@gmail.com>
2020-06-19 04:21:00 +0530

    Add solution for problem `B - Two Buttons` Link: https://codeforces.com/contest/520/problem/B Tags: dfs and similar, graphs, greedy, implementation, math, shortest paths, *1400 Ref: https://codeforces.com/contest/520/submission/84279481

---
+++ codeforces/520/B.cpp
@@ -0,0 +1,133 @@
+
+// Problem : B. Two Buttons
+// Contest : Codeforces - Codeforces Round #295 (Div. 2)
+// URL : https://codeforces.com/problemset/problem/520/B
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl                    '\n'
+#define rep()                   for(LL i = 0 ; i < n ; i++)
+#define f(i,s,n)                for(LL i = s ; i < n ; i++)
+#define fe(i,s,n)               for(LL i = s ; i <= n ; i++)
+#define rfe(i,s,n)              for(LL i = s ; i >= n ; i--)
+#define auto(m)                 for(auto &it:m)
+#define f2(it,v)                for(it = v.begin(); it != v.end(); ++it)
+#define fori(it, x)             for (__typeof((x).begin()) it = (x).begin(); it != (x).end(); it++)
+#define ip(n,a)                 LL n;cin>>n;LL a[n];rep(){cin>>a[i];}
+#define op(n,a)                 f(i,0,n){cout<<a[i]<<" ";}cout<<endl;
+#define i2p(n,k,a)              LL n,k;cin>>n>>k;LL a[n];rep{cin>>a[i];}
+#define TCs(x)                  LL x; cin>>x; while(x--)
+#define TC(x)                   LL x=1; while(x--)
+#define mp                      make_pair
+#define pb                      push_back
+#define ff                      first
+#define ss                      second
+#define all(v)                  v.begin(), v.end()
+#define mem(a,x)                memset(a,x,sizeof(a))
+#define Max(x,y,z)              max(x,max(y,z))
+#define Min(x,y,z)              min(x,min(y,z))
+#define fastIO                  ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ps(x)                   std::cout << std::fixed; std::cout << std::setprecision(x);
+#define clz(a)                  __builtin_clz(a) // count leading zeroes (geeksforgeeks.org/builtin-functions-gcc-compiler/)
+#define ctz(a)                  __builtin_ctz(a) // count trailing zeroes
+#define popc(a)                 __builtin_popcount(a) // count set bits (for ints only diff for ll)
+#define GCD(A,B)                __gcd(A,B)
+#define trace1(x)                cout<<#x<<": "<<x<<endl
+#define trace2(x, y)             cout<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cout<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+//Typedefs:-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+typedef string                  S;
+typedef long long               LL;
+typedef long double             LD;
+typedef unsigned long long      ULL;
+typedef vector<long long>       VL;
+typedef vector<VL>              VVL;
+typedef vector<string>          VS;
+typedef vector<char>            VC;
+typedef pair<LL,LL>             PLL;
+typedef pair<string,LL>         PSL;
+typedef pair<LL,string>         PLS;
+typedef pair<char,LL>           PCL;
+typedef pair<LL,char>           PLC;
+typedef pair<string,string>     PSS;
+typedef pair<char,char>         PCC;
+typedef vector<PLL>             VPLL;
+typedef vector<PSL>             VPSL;
+typedef vector<PLS>             VPLS;
+typedef vector<PCL>             VPCL;
+typedef vector<PLC>             VPLC;
+typedef vector<PSS>             VPSS;
+typedef vector<PCC>             VPCC;
+typedef map<LL,LL>              MLL;
+typedef map<string,LL>          MSL;
+typedef map<char,LL>            MCL;
+typedef set<LL>                 SL;
+typedef set<string>             SS;
+typedef set<char>               SC;
+
+const long long N=(long long)(1e5+1);
+const long long MAX=(long long)(1e6+1);	
+const long long MOD=(long long)(1e9+7);
+const long long INF=(long long)(2e18);
+
+VL ans,graph[MAX];
+bool vis[MAX],vis_g[MAX];
+queue<LL> q;
+LL n,m,dis[MAX];
+void extend_g(LL x){
+	if(x!=m&&x>0&&!vis_g[x]){
+		vis_g[x]=true;
+		graph[x].pb(x-1);
+		graph[x].pb(2*x);
+		if(x<m){extend_g(2*x);}
+		extend_g(x-1);
+	}
+}
+void bfs(LL x){
+	vis[x]=true;
+	q.push(x);
+	while(!q.empty()){
+		LL front=q.front();
+		q.pop();
+		for(auto it:graph[front]){
+			if(!vis[it]){
+				vis[it]=true;
+				dis[it]=dis[front]+1;
+				q.push(it);
+			}
+		}
+	}	
+}
+void solve()
+{
+	//BFS solution
+	cin>>n>>m;
+	if(n==m){cout<<0<<endl;}
+	else{
+		extend_g(n);
+		/*f(i,0,13){
+			//cout<<graph[i][0]<<" : ";
+			cout<<i<<" : ";
+			f(j,0,(LL)graph[i].size()){
+				cout<<graph[i][j]<<" ";
+			}
+			cout<<endl;
+		}*/
+		bfs(n);
+		cout<<dis[m]<<endl;		
+	}	
+}
+int main()
+{
+            fastIO
+            TC(t){solve();}
+            return 0;
+}
