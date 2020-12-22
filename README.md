bba774b9a8e84e8c8c0cd12b99dba4d603cbeb15
Shahil Singh <shahilsingh5501@gmail.com>
2020-12-22 10:22:00 +0530

    Add solution for problem `D - Complete Tripartite` Link: https://codeforces.com/contest/1228/problem/D Tags: brute force, constructive algorithms, graphs, hashing, implementation, *1900 Ref: https://codeforces.com/contest/1228/submission/102013041

--- README.md
+++ README.md
@@ -12,8 +12,9 @@ A repository to keep track of problem solving practise, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-884 | [A - Domino piling](https://codeforces.com/contest/50/problem/A) | [GNU C++17](./codeforces/50/A.cpp) | `greedy` `math` `*800` | Dec/23/2020 15:28 | 
-883 | [A - Equation](https://codeforces.com/contest/1269/problem/A) | [GNU C++17 (64)](./codeforces/1269/A.cpp) | `brute force` `math` `*800` | Dec/22/2020 22:39 | 
+885 | [A - Domino piling](https://codeforces.com/contest/50/problem/A) | [GNU C++17](./codeforces/50/A.cpp) | `greedy` `math` `*800` | Dec/23/2020 15:28 | 
+884 | [A - Equation](https://codeforces.com/contest/1269/problem/A) | [GNU C++17 (64)](./codeforces/1269/A.cpp) | `brute force` `math` `*800` | Dec/22/2020 22:39 | 
+883 | [D - Complete Tripartite](https://codeforces.com/contest/1228/problem/D) | [GNU C++17 (64)](./codeforces/1228/D.cpp) | `brute force` `constructive algorithms` `graphs` `hashing` `implementation` `*1900` | Dec/22/2020 10:22 | 
 882 | [D - Complete Tripartite](https://codeforces.com/contest/1228/problem/D) | [GNU C++17](./codeforces/1228/D.cpp) | `brute force` `constructive algorithms` `graphs` `hashing` `implementation` `*1900` | Dec/22/2020 05:08 | 
 881 | [B - Fair Numbers](https://codeforces.com/contest/1465/problem/B) | [GNU C++17 (64)](./codeforces/1465/B.cpp) | `number theory` | Dec/21/2020 06:14 | 
 880 | [B - Fair Numbers](https://codeforces.com/contest/1465/problem/B) | [GNU C++17 (64)](./codeforces/1465/B.cpp) | `number theory` | Dec/21/2020 06:13 | 
--- codeforces/1228/D.cpp
+++ codeforces/1228/D.cpp
@@ -1,196 +1,85 @@
-// Problem: D. Complete Tripartite
-// Contest: Codeforces - Codeforces Round #589 (Div. 2)
-// URL: https://codeforces.com/contest/1228/problem/D
-// Memory Limit: 256 MB
-// Time Limit: 2000 ms
-// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+//bitset solution
+//https://codeforces.com/blog/entry/70162?#comment-546675
+//Pending
 
-//g++ -Wall
-//g++ -static -DONLINE_JUDGE -Wl,--stack=268435456 -O2 -std=c++17
 #include <bits/stdc++.h>
+#include <stdio.h>
+ 
 using namespace std;
-#pragma GCC optimize("-Ofast")
-#pragma GCC optimize("trapv")
-//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-//#pragma GCC optimize("-O2")
-//#pragma GCC target("AVX")
-#define fastIO                  ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define endl                    '\n'
-#define rep()                   for(LL i = 0 ; i < n ; i++)
-#define f(i,s,n)                for(LL i = s ; i < n ; i++)
-#define fe(i,s,n)               for(LL i = s ; i <= n ; i++)
-#define rfe(i,s,n)              for(LL i = s ; i >= n ; i--)
-#define each(it,x)		for(auto &it:x)
-#define f2(it,v)                for(it = v.begin(); it != v.end(); ++it)
-#define fori(it, x)             for (__typeof((x).begin()) it = (x).begin(); it != (x).end(); it++)
-#define TCs(x)                  LL x; cin>>x; while(x--)
-#define TC(x)                   LL x=1; while(x--)
-#define google(x)		cout<<"Case #"<<x<<": ";
-#define mp                      make_pair
-#define pb                      push_back
-#define pf                      push_front
-#define ff                      first
-#define ss                      second
-#define all(v)                  v.begin(), v.end()
-#define mem(a,x)                memset(a,x,sizeof(a))
-#define imin                    INT_MIN
-#define imax                    INT_MAX
-#define sz(x)       		(LL)x.size()
-#define ps(x)                   std::cout << std::fixed; std::cout << std::setprecision(x);
-#define clz(a)                  __builtin_clz(a) // count leading zeroes (geeksforgeeks.org/builtin-functions-gcc-compiler/)
-#define ctz(a)                  __builtin_ctz(a) // count trailing zeroes
-#define popc(a)                 __builtin_popcount(a) // count set bits (for ints only diff for ll)
-#define d1(x)                   cout<<#x<<": "<<x<<endl
-#define d2(x, y)                cout<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
-#define d3(x, y, z)             cout<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
-#define d4(a, b, c, d)          cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
-#define d5(a, b, c, d, e)       cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
-#define d6(a, b, c, d, e, f)    cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+ 
+// #define int long long
+ 
+void solve();
 
-typedef string                  S;
-typedef long long               LL;
-typedef long double             LD;
-typedef unsigned long long      ULL;
-typedef vector<long long>       VL;
-typedef vector<VL>              VVL;
-typedef vector<string>          VS;
-typedef vector<char>            VC;
-typedef pair<LL,LL>             PLL;
-typedef pair<string,LL>         PSL;
-typedef pair<LL,string>         PLS;
-typedef pair<char,LL>           PCL;
-typedef pair<LL,char>           PLC;
-typedef pair<string,string>     PSS;
-typedef pair<char,char>         PCC;
-typedef vector<PLL>             VPLL;
-typedef vector<PSL>             VPSL;
-typedef vector<PLS>             VPLS;
-typedef vector<PCL>             VPCL;
-typedef vector<PLC>             VPLC;
-typedef vector<PSS>             VPSS;
-typedef vector<PCC>             VPCC;
-typedef map<LL,LL>              MLL;
-typedef map<string,LL>          MSL;
-typedef map<LL,string>          MLS;
-typedef map<char,LL>            MCL;
-typedef map<LL,char>            MLC;
-typedef map<char,char>          MCC;
-typedef map<string,string>      MSS;
-typedef set<LL>                 SL;
-typedef set<string>             SS;
-typedef set<char>               SC;
-typedef priority_queue<LL>	PQLL;
-typedef priority_queue<LL,VL,greater<LL>>	mPQLL;
-
-//Time
-#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
-
-#define time__(d) \
-    for ( \
-        auto blockTime = make_pair(chrono::high_resolution_clock::now(), true); \
-        blockTime.second; \
-        debug("%s: %lld ms\n", d, chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - blockTime.first).count()), blockTime.second = false \
-    )           
-// #define time__(d) for(long blockTime = 0; (blockTime == 0 ? (blockTime=clock()) != 0 : false); debug("%s time : %.4fs", d, (double)(clock() - blockTime) / CLOCKS_PER_SEC))
-
-//Functions
-LL power(LL x,LL y){if(y == 0){return 1;} LL p = power(x, y/2); p = (p * p); return (y%2 == 0)? p : (x * p);}  
-LL powm(LL x,LL y,LL m){LL ans=1,r=1;x%=m;while(r>0&&r<=y){if(r&y){ans*=x;ans%=m;} r<<=1;x*=x;x%=m;}return ans;}   
-LL gcd(LL a,LL b) { if (b == 0){return a;} return gcd(b, a % b);}  
-LL lcm(LL a,LL b){LL lar = max(a, b);LL small = min(a, b);for (LL i = lar; ; i += lar) { if (i % small == 0){ return i;} } }
-bool sortbysec(const pair<LL,LL> &a, const pair<LL,LL> &b) {  return (a.ss<b.ss); }
-bool sortbydec(const pair<LL,LL> &a, const pair<LL,LL> &b) {  return (a.ff>b.ff); }
-void dex(int a){ if(a==1){cout<<"YES";} if(a==2){cout<<"NO";} if(a==3){cout<<"Yes";} if(a==4){cout<<"No";}}
-void line(LL x){fe(i,1,x){cout<<"-";}cout<<endl;}
-//Use LLONG_MAX & LLONG_MIN
-const long long N=(long long)(3e5+1);
-const long long MOD=(long long)(1e9+7);
-const long long INF=(long long)(2e18);
-vector< SL > v(N); 
-VL gr(N,-1);
-VL grs[4];
-void solve(LL tc)
-{
-	//Editorial
-	LL n,m;
-	cin>>n>>m;
-	fe(i,1,m){
-		LL x,y;
-		cin>>x>>y;
-		v[x].insert(y);
-		v[y].insert(x);
+signed main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	#ifdef LOCAL
+		freopen("input.txt", "r", stdin);
+	#endif
+	int t = 1; 
+	// cin >> t;
+	int tt = t;
+	while (t--) { 
+		solve();
 	}
-	
-	fe(g,1,3){
-		
-		LL f;
-		for(f=1;f<=n;f++){
-			if(gr[f]==-1){
+	return 0;
+}
+const int N = 1e5 + 5;
+vector<int> adj[N];
+vector<bitset<N>> v;
+int which[N];
+int n, m;
+void solve() {
+	scanf("%d%d", &n, &m);
+	for (int i = 1; i <= m; i += 1) {
+		int a, b;
+		scanf("%d%d", &a, &b);
+		--a, --b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	for (int i = 0; i < n; i += 1) {
+		bitset<N> new_b;
+		for (auto j : adj[i]) {
+			new_b[j] = 1;
+		}
+		bool found = false;
+		for (int j = 0; j < (int) v.size(); j += 1) {
+			if (new_b == v[j]) {
+				which[i] = j;
+				found = true;
 				break;
 			}
-		}
-		if(f==n+1){
-			cout<<-1<<endl;
+		} 
+		if (!found and v.size() == 3) {
+			printf("-1");
 			return;
-		}
-		
-		gr[f]=g;
-		for(LL s=1;s<=n;s++){
-			if(f!=s && gr[s]==-1 && v[f].find(s)==v[f].end()){
-				gr[s]=g;
-			}
-		}
+		} 
+		if (found)
+			continue;
+		v.push_back(new_b);
+		which[i] = (int) v.size() - 1;
 	}
-	
-	for(LL now=1;now<=n;now++){
-		if(gr[now]==-1){
-			cout<<-1<<endl;
-			return;
-		}
-		else{
-			grs[gr[now]].pb(now);
-		}
-	}
-	
-	LL fe=0;
-	for(LL g1=1;g1<=3;g1++){
-		for(LL g2=g1+1;g2<=3;g2++){
-			for(auto v1:grs[g1]){
-				for(auto v2:grs[g2]){
-					if(v[v1].find(v2)==v[v1].end()){
-						cout<<-1<<endl;
-						return;
-					}
-					else{
-						fe++;
-					}
-				}
-			}
-		}
-	}
-	
-	if(fe!=m){
-		cout<<-1<<endl;
+	if ((int) v.size() <= 2) {
+		cout << -1;
 		return;
 	}
-	fe(now,1,n){
-		cout<<gr[now]<<" ";
+	for (int i = 0; i < n; i += 1) {
+		if (v[which[i]][i]) {
+			cout << -1;
+			return;
+		}
+		for (int j = 0; j < 3; j += 1) {
+			if (j == which[i])
+				continue;
+			if (!v[j][i]) {
+				cout << -1;
+				return;
+			}
+		}
 	}
-}
-int main()
-{
-// #ifndef ONLINE_JUDGE
-        // freopen("input.txt","r",stdin);
-        // freopen("output.txt","w",stdout);
-// #endif
-        fastIO
-        LL cnt=1;
-        TC(t){
-        	// google(cnt);
-                time__("solve"){
-	                solve(cnt);
-	        }
-                cnt++;
-        }
-        return 0;
-}
+	for (int i = 0; i < n; i++) {
+		cout << which[i] + 1 << ' ';
+	}
+}		
