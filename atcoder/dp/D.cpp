970065ae0f5d2bd3f57db3a4914ae0b842ce617c
Shahil Singh <shahilsingh5501@gmail.com>
2021-01-18 00:51:00 +0530

    Add solution for problem `D - Knapsack 1` Link: https://atcoder.jp/contests/dp/tasks/dp_d Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/19500346

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -17,33 +17,50 @@ const long long MOD=(long long)(1e9+7);
 int n,w;
 int val[101],wt[N];
 int dp[101][N];
+int knapsack(int n,int w){
+	// cout<<n<<" "<<w<<endl;
+	if(n==0 || w==0){
+		return 0;
+	}
+	if(dp[n][w]!=-1){
+		return dp[n][w];
+	}
+	if(wt[n]<=w){
+		int op1=val[n]+knapsack(n-1,w-wt[n]);
+		int op2=knapsack(n-1,w);
+		dp[n][w]=max(op1,op2);
+		// cout<<n<<" "<<w<<" : "<<op1<<" "<<op2<<" : "<<dp[n][w]<<endl;
+		return dp[n][w];
+	}
+	else{
+		dp[n][w]=knapsack(n-1,w);
+		// cout<<n<<" "<<w<<" : "<<dp[n][w]<<endl;
+		return dp[n][w];
+	}
+}
 void solve(){
-        cin>>n>>w;
-        for(int i=1;i<=n;i++){
-                cin>>wt[i]>>val[i];
-        }
-        for(int i=1;i<=n;i++){
-                for(int j=1;j<=w;j++){
-                        if(wt[i]<=j){
-                                dp[i][j]=max(val[i]+dp[i-1][j-wt[i]],dp[i-1][j]);
-                        }
-                        else{
-                                dp[i][j]=dp[i-1][j];
-                        }
-                        // cout<<i<<" "<<j<<" "<<dp[i][j]<<endl;
-                }
-        }
-        cout<<dp[n][w]<<endl;
-        // for(int i=0;i<=n;i++){
-        //         for(int j=0;j<=w;j++){
-        //                 cout<<dp[i][j]<<" ";
-        //         }
-        //         cout<<endl;
-        // }
+	cin>>n>>w;
+	for(int i=1;i<=n;i++){
+		cin>>wt[i]>>val[i];
+	}
+	for(int i=0;i<=n;i++){
+		for(int j=0;j<=w;j++){
+			if(i!=0 and j!=0){
+				dp[i][j]=-1;
+			}
+		}
+	}
+	cout<<knapsack(n,w)<<endl;
+	// for(int i=0;i<=n;i++){
+	// 	for(int j=0;j<=w;j++){
+	// 		cout<<dp[i][j]<<" ";
+	// 	}
+	// 	cout<<endl;
+	// }
 }           
 int32_t main()
 {
-    fastIO
+	fastIO
 #ifndef ONLINE_JUDGE
         freopen("input.txt","r",stdin);
         freopen("output.txt","w",stdout);
@@ -52,9 +69,9 @@ int32_t main()
         // cin>>tc;
         while(tc--){
                 time__("solve"){
-                    solve();
-            }
+	                solve();
+	        }
         }
-    
+	
         return 0;
 }
